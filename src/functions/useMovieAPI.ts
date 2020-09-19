2fb4de2e6fb0702642f291aafb46dd140866cd61
Alex Chiu <alexchiu11@gmail.com>
2020-09-19 18:55:21 +0100

    Rendered API call and setup TS for reponse call

--- src/functions/useMovieAPI.ts
+++ src/functions/useMovieAPI.ts
@@ -1,15 +1,19 @@
 import { ref } from "vue";
 import axios from "axios";
+import { TrendingMovies, Movie } from "../types";
 
-const movieList = ref([]);
+const movieList = ref<Movie[]>([]);
 const API_KEY = process.env.VUE_APP_API_KEY;
 
-async function getMovies() {
+async function getMovies(): Promise<TrendingMovies> {
   const trendingMovies = await axios.get(
     `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`
   );
 
   console.log("got movies", trendingMovies.data);
+  (trendingMovies.data as TrendingMovies).results.forEach((movie) => {
+    movieList.value.push(movie);
+  });
   return trendingMovies.data;
 }
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,22 @@
   <div class="home">
     <h1 class="text-center p-5">Movie Night</h1>
     <h1 class="text-center p-5">What Do You Want to Watch Tonight?</h1>
+    <div v-for="(movie, key) in movieList" :key="key">
+      {{ movie.original_title }}
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted } from "vue";
-import { getMovies } from "../functions/useMovieAPI";
+import { getMovies, movieList } from "../functions/useMovieAPI";
 export default defineComponent({
   setup() {
     onMounted(() => {
       getMovies();
     });
+
+    return { movieList };
   },
 });
 </script>
