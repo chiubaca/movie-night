0bdd55715eb458b46e7cc16eac2d554e7e9e9d69
Alex Chiu <alexchiu11@gmail.com>
2020-09-20 19:06:11 +0100

    Setup MovieCard component with some basic styling

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="movie-card" class="">
+    <span>{{ movie.title }}</span>
+    <img
+      :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
+      :alt="movie.title"
+      class="w-40"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import { Movie } from "../types";
+
+export default defineComponent({
+  props: {
+    movie: {
+      type: Object as PropType<Movie>,
+      required: true,
+    },
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="home">
-    <h1 class="text-center p-5">Movie Night</h1>
-    <h1 class="text-center p-5">What Do You Want to Watch Tonight?</h1>
-    <div v-for="(movie, key) in movieList" :key="key">
-      {{ movie.original_title }}
+    <h1 class="text-center p-5">What 2 Watch</h1>
+    <h2 class="text-center p-5">
+      Find a movie to watch based on something you enjoyed recently
+    </h2>
+    <div id="movies-container" class="flex flex-wrap flex-1 justify-evenly">
+      <MovieCard v-for="(movie, key) in movieList" :key="key" :movie="movie" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted } from "vue";
+import MovieCard from "../components/MovieCard.vue";
 import { getMovies, movieList } from "../functions/useMovieAPI";
 export default defineComponent({
+  components: {
+    MovieCard,
+  },
   setup() {
     onMounted(() => {
       getMovies();
