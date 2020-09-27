4f8e3c98230feefbc99328f4210e5e9f4739a1e4
Alex Chiu <alexchiu11@gmail.com>
2020-09-27 19:51:26 +0100

    Experimenting with data from MovieDB API.
    TODO: workout how to make multiple API calls based off array of movieIDs

--- functions/movie-recomendations.ts
+++ functions/movie-recomendations.ts
@@ -1,5 +1,6 @@
+/* eslint-disable no-inner-declarations */
 import { APIGatewayProxyEvent, APIGatewayProxyCallback } from "aws-lambda";
-// import axios from "axios";
+import axios from "axios";
 
 export const handler = async function (
   event: APIGatewayProxyEvent,
@@ -7,21 +8,23 @@ export const handler = async function (
   callback: APIGatewayProxyCallback
 ): Promise<void> {
   try {
-    if (event.httpMethod === "GET") {
-      // const API_KEY = process.env.VUE_APP_MOVIE_DB_API_KEY;
+    if (event.httpMethod === "POST") {
+      const API_KEY = process.env.VUE_APP_MOVIE_DB_API_KEY;
+      // const recomendationsPayload: any[] = [];
 
-      // extract post id from query string
-      const queryParam = event?.queryStringParameters;
+      // extract movie Ids from body payload
+      const movieIds: { ids: number[] } = await JSON.parse(
+        event.body as string
+      );
 
-      // const trendingMovies = await axios.get(
-      //   `https://api.themoviedb.org/3/movie/${movieID}/recommendations?api_key=${API_KEY}`
-      // );
-      console.log({ queryParam });
+      const recomendations = await axios.get(
+        `https://api.themoviedb.org/3/movie/${movieIds.ids[0]}/recommendations?api_key=${API_KEY}`
+      );
 
       return callback(null, {
         statusCode: 200,
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ msg: queryParam }),
+        body: JSON.stringify({ msg: recomendations.data }),
       });
     } else {
       return callback(null, {
