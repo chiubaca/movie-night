a0e972e69a85e54e4ea885a22474e7f28bc38163
Alex Chiu <alexchiu11@gmail.com>
2020-09-29 20:55:38 +0100

    Implemented  error handling and API call to recomended movies

--- functions/movie-recomendations.ts
+++ functions/movie-recomendations.ts
@@ -1,22 +1,23 @@
 import { APIGatewayProxyEvent, APIGatewayProxyCallback } from "aws-lambda";
+import axios from "axios";
+
+// Dont let axios throw an error on 4XX errors so we can handle manually
+axios.defaults.validateStatus = (status: number): boolean => status <= 500;
 
 export const handler = async function (
   event: APIGatewayProxyEvent,
-  _context: any,
+  _context: unknown,
   callback: APIGatewayProxyCallback
 ): Promise<void> {
   try {
     if (event.httpMethod === "GET") {
-      // const API_KEY = process.env.VUE_APP_MOVIE_DB_API_KEY;
-      console.log("working...");
+      const API_KEY = process.env.VUE_APP_MOVIE_DB_API_KEY;
 
-      // extract movie Ids from query param
-      const queryParam = event.queryStringParameters;
-      const movieId = queryParam.id as unknown;
+      // Extract movie Ids from query param
+      const queryParam = event.queryStringParameters?.id as string;
 
-      console.log("what are the params?", movieId);
-
-      if (!movieId) {
+      // Check if id param was provided if not throw error
+      if (!queryParam) {
         return callback(null, {
           statusCode: 401,
           headers: { "Content-Type": "application/json" },
@@ -26,10 +27,44 @@ export const handler = async function (
         });
       }
 
+      // Type check on query param to ensure it is a number
+      const movieId: number | false = isNaN(Number(queryParam))
+        ? false
+        : Number(queryParam);
+
+      // Throw error if provided id was not a number
+      if (movieId === false) {
+        console.log("empy id", queryParam);
+        return callback(null, {
+          statusCode: 401,
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            error: "Not a valid movie ID",
+          }),
+        });
+      }
+
+      // Make API call to MovieDB
+      const recommendedMovies = await axios.get(
+        `https://api.themoviedb.org/3/movie/${movieId}/recommendations?api_key=${API_KEY}`
+      );
+
+      // Return error if MovieDB did not find a movie from ID
+      if (recommendedMovies.data.success === false) {
+        return callback(null, {
+          statusCode: 401,
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            error: "Not a valid movie ID",
+          }),
+        });
+      }
+
+      // Happy path - return back results from MovieDB
       return callback(null, {
         statusCode: 200,
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ msg: "movie Id :" + movieId }),
+        body: JSON.stringify({ results: recommendedMovies.data.results }),
       });
     } else {
       return callback(null, {
@@ -39,7 +74,7 @@ export const handler = async function (
       });
     }
   } catch (error) {
-    console.error("There was an error", error);
+    console.error("There was a fatal error", error);
     callback(null, {
       statusCode: 400,
       headers: { "Content-Type": "application/json" },
