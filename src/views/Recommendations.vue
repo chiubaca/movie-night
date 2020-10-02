88072080b543cf63da14bc55d8155465b7d50c15
Alex Chiu <alexchiu11@gmail.com>
2020-10-02 20:54:53 +0100

    Moved recomended movies state into useCardDeck

--- src/views/Recommendations.vue
+++ src/views/Recommendations.vue
@@ -2,25 +2,29 @@
   <main class="text-center">
     <h3 class="text-xl p-5">Want to watch any of these?</h3>
     <div id="deck" class="flex justify-around items-baseline h-64">
-      <div id="yes" class="">
-        <span class="bg-green-400 p-5 rounded-lg">Yeah!😀</span>
-      </div>
+      <button class="bg-green-400 p-5 rounded-lg" @click="addToWatchList">
+        Yeah!😀
+      </button>
+
       <div id="recommended-movies-container" class="w-1/4">
-        <MovieCardsStack :movies="recomendedMovies" />
+        <MovieCardsStack :movies="recommendedMovies" />
       </div>
-      <div id="no" class="">
-        <span class="bg-red-400 p-5 rounded-lg">Nah🥱</span>
-      </div>
+
+      <button class="bg-red-400 p-5 rounded-lg">Nah🥱</button>
     </div>
   </main>
 </template>
 
 <script lang="ts">
-import { onMounted, ref } from "vue";
+import { onMounted } from "vue";
 import { useRoute } from "vue-router";
 import MovieCardsStack from "@/components/MovieCardsStack.vue";
-import { getMovieRecomendation } from "../functions/useMovieAPI";
-import { Movie } from "../types";
+import { getMovieRecomendation } from "@/functions/useMovieAPI";
+import {
+  recommendedMovies,
+  populateRecommendedMovies,
+  addToWatchList,
+} from "@/functions/useCardDeck";
 
 export default {
   components: {
@@ -28,19 +32,16 @@ export default {
   },
   setup() {
     const route = useRoute();
-    const recomendedMovies = ref<Movie[]>([]);
 
     onMounted(() => {
       const movieId = route.query.id;
       getMovieRecomendation(movieId).then((resp) => {
         console.log("GOT MOVIES", resp);
-        resp.forEach((movie) => {
-          recomendedMovies.value.push(movie);
-        });
+        populateRecommendedMovies(resp);
       });
     });
 
-    return { recomendedMovies };
+    return { recommendedMovies, addToWatchList };
   },
 };
 </script>
