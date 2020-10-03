5384bbaa908f8e1e97c9f00a4ffbc49ec1033f03
Alex Chiu <alexchiu11@gmail.com>
2020-10-03 21:21:32 +0100

    - Set position of move card with tilt effect - emit accept or reject events depending where card is - reset position if not in a valid position - remove interact js handles once movie it unmounted

--- src/components/SwipeableMovieCard.vue
+++ src/components/SwipeableMovieCard.vue
@@ -34,27 +34,37 @@ export default defineComponent({
       required: true,
     },
   },
-  setup() {
+  emits: ["add-movie", "reject-movie"],
+  setup(props, { emit }) {
     const staticVals = {
       interactMaxRotation: 15,
-      interactOutOfSightXCoordinate: 500,
+      interactOutOfSightXCoordinate: 1000,
       interactOutOfSightYCoordinate: 600,
       interactYThreshold: 150,
       interactXThreshold: 100,
     };
 
+    // reference of movie card element for interact.js
     const interactElement = ref();
+
+    // Movie card position
     const position = reactive({
       x: 0,
       y: 0,
       rotation: 0,
     });
 
+    /**
+     * Styling property
+     */
     const transformString = computed(() => {
-      // try destructuring later
-      return `translate3D(${position.x}px, ${position.y}px, 0) rotate(${position.rotation}deg)`;
+      const { x, y, rotation } = position;
+      return `translate3D(${x}px, ${y}px, 0) rotate(${rotation}deg)`;
     });
 
+    /**
+     * Helper to quickly set the translated postion of movie card works in tandem with transformString
+     */
     function interactSetPosition(coordinates: {
       x: number;
       y: number;
@@ -65,30 +75,67 @@ export default defineComponent({
       position.rotation = coordinates.rotation;
     }
 
-    onMounted(() => {
-      console.log("Mounted!");
+    /**
+     * Reset position of movie card
+     */
+    function resetCardPosition() {
+      interactSetPosition({ x: 0, y: 0, rotation: 0 });
+    }
 
+    /**
+     * Remove interact.js properties  and events from reference element
+     */
+    function interactUnsetElement() {
+      interact(interactElement.value).unset();
+    }
+
+    /**
+     * Handle conditions when cards have been moved into specfic position
+     */
+    function handleMoviePlacement(interaction: string) {
+      interactUnsetElement();
+      switch (interaction) {
+        case "ACCEPT_CARD":
+          console.log("YES");
+          emit("add-movie");
+          break;
+        case "REJECT_CARD":
+          console.log("NO");
+          emit("reject-movie");
+          break;
+      }
+    }
+
+    onMounted(() => {
       interact(interactElement.value).draggable({
+        inertia: true,
         listeners: {
-          start() {
-            // console.log("interact event", event.type, event.target);
-          },
           move(event: Interact.InteractEvent) {
             const { interactMaxRotation, interactXThreshold } = staticVals;
 
+            // Bind the cursor event position to vue state
             const x = (position.x += event.dx);
             const y = (position.y += event.dy);
-            const rotation = interactMaxRotation * (x / interactXThreshold);
 
-            // event.target.style.transform = `translate(${position.x}px, ${position.y}px)`;
+            // Fun maths to make the card tilt depending which it's being dragged
+            const rotation = interactMaxRotation * (x / interactXThreshold);
             interactSetPosition({ x, y, rotation });
           },
+        },
+        onend() {
+          // Swipe to the right to reject the movie
+          if (position.x > staticVals.interactXThreshold)
+            handleMoviePlacement("REJECT_CARD");
+          // Swipe to the left to accept the movie
+          else if (position.x < -staticVals.interactXThreshold)
+            handleMoviePlacement("ACCEPT_CARD");
+          else resetCardPosition();
         },
       });
     });
 
     onBeforeUnmount(() => {
-      interact(interactElement.value).unset();
+      interactUnsetElement();
     });
 
     return { position, interactElement, transformString };
