3c4d12682944de61dc09a86bb6a1c0ab066c9375
Alex Chiu <alexchiu11@gmail.com>
2020-10-09 17:07:16 +0100

    Add navigation link to TV shows

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <nav id="nav" class="p-5 text-center">
     <router-link to="/movies">
-      <span class="hover:border-black border-b-4">Films 🎥</span>
+      <span class="hover:border-black border-b-4">Movies 🎥</span>
+    </router-link>
+    |
+    <router-link to="/tv">
+      <span class="hover:border-black border-b-4">TV Shows 📺</span>
     </router-link>
   </nav>
   <h1 class="text-center text-2xl font-bold font-serif">Whatch - /wɒtʃ/</h1>
