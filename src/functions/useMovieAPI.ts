2d73b09d3fe5cf3b06ceda0b7e6b3f6cb5908df5
Alex Chiu <alexchiu11@gmail.com>
2020-10-18 17:09:17 +0100

    - Refactor to use Show type - Added get tv shows api call - Added get tv recomendation api call

--- src/functions/useMovieAPI.ts
+++ src/functions/useMovieAPI.ts
@@ -1,8 +1,9 @@
 import { ref } from "vue";
 import axios from "axios";
-import { TrendingMovies, Movie } from "../types";
+import { TrendingShows, Show } from "../types";
 
-const movieList = ref<Movie[]>([]);
+const movieList = ref<Show[]>([]);
+const tvList = ref<Show[]>([]);
 let baseURL: string;
 
 /**
@@ -20,19 +21,31 @@ if (process.env.NODE_ENV === "production") {
 /**
  * Get top 20 trending movies from MovieDB API
  */
-async function getMovies(): Promise<TrendingMovies> {
+async function getMovies(): Promise<TrendingShows> {
   const trendingMovies = await axios.get(`${baseURL}/movies`);
 
-  (trendingMovies.data as TrendingMovies).results.forEach((movie) => {
+  (trendingMovies.data as TrendingShows).results.forEach((movie) => {
     movieList.value.push(movie);
   });
   return trendingMovies.data;
 }
 
 /**
+ * Get top 20 trending TV shows from MovieDB API
+ */
+async function getTvShows(): Promise<TrendingShows> {
+  const trendingTv = await axios.get(`${baseURL}/tv`);
+
+  (trendingTv.data as TrendingShows).results.forEach((tvShow) => {
+    tvList.value.push(tvShow);
+  });
+  return trendingTv.data;
+}
+
+/**
  * Get movie recomendations
  */
-async function getMovieRecomendation(movieId: any): Promise<Movie[]> {
+async function getMovieRecomendation(movieId: any): Promise<Show[]> {
   try {
     const recomendations = await axios.get(
       `${baseURL}/movie-recomendations?id=${movieId}`
@@ -49,4 +62,31 @@ async function getMovieRecomendation(movieId: any): Promise<Movie[]> {
   }
 }
 
-export { movieList, getMovies, getMovieRecomendation };
+/**
+ * Get movie recomendations
+ */
+async function getTvRecomendation(tvId: any): Promise<Show[]> {
+  try {
+    const recomendations = await axios.get(
+      `${baseURL}/tv-recomendations?id=${tvId}`
+    );
+    if (recomendations.data.error) {
+      console.log("no data for this tv ID");
+      return [];
+    }
+
+    return recomendations.data.results;
+  } catch (err) {
+    console.error("There was problem with the API", err);
+    return [];
+  }
+}
+
+export {
+  movieList,
+  tvList,
+  getMovies,
+  getTvShows,
+  getMovieRecomendation,
+  getTvRecomendation,
+};
