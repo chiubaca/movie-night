db2cbdd51c40a0f9975a91e2b5dc14041cb5a972
Alex Chiu <alexchiu11@gmail.com>
2020-10-23 20:11:21 +0100

    Watchlist view, not working becasue TV and Movies have different state. Big refactor required

--- src/views/WatchList.vue
+++ src/views/WatchList.vue
@@ -1,13 +1,17 @@
 <template>
-  <h1>Your watch list</h1>
+  <h1 class="text-center">Your watch list</h1>
+  <div v-for="(show, index) in watchList" :key="index">
+    {{ show.original_name }}
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { watchList } from "@/functions/useWatchList";
 
 export default defineComponent({
   setup() {
-    return {};
+    return { watchList };
   },
 });
 </script>
