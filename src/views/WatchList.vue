4120afeedc74844b0474cefaf30e40edecc716ff
Alex Chiu <alexchiu11@gmail.com>
2020-10-26 19:16:41 +0000

    Messing around with the styling of watch list for both movie and tv shows

--- src/views/WatchList.vue
+++ src/views/WatchList.vue
@@ -1,11 +1,43 @@
 <template>
-  <h2 class="text-center">Movies Watch List</h2>
-  <div v-for="(movie, key) in movieWatchList" :key="key" class="text-center">
-    <MoviePosterCard :key="key" :index="key" :movie="movie" />
+  <h2 class="text-center text-lg">Movies Watch List</h2>
+  <div v-if="movieWatchList.length === 0" class="text-center">
+    You have nothing in your TV watch list yet...
+  </div>
+  <div
+    v-else
+    id="movies-container"
+    class="flex flex-wrap flex-1 justify-evenly"
+  >
+    <div
+      v-for="(movie, key) in movieWatchList"
+      id="movie-card"
+      :key="key"
+      class="w-1/2 md:w-1/4 lg:w-1/5"
+    >
+      <div id="mask" class="bg-red-700 relative m-4">
+        <label>
+          <h2
+            class="hover:text-white text-center absolute hover:bg-orange-600 hover:bg-opacity-50 w-full h-full text-transparent cursor-pointer"
+          >
+            {{ movie.title }}
+          </h2>
+
+          <img
+            loading="lazy"
+            :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
+            :alt="movie.title"
+            class=""
+          />
+        </label>
+      </div>
+    </div>
   </div>
 
-  <h2 class="text-center">TV Watch List</h2>
-  <div v-for="(tv, key) in tvWatchList" :key="key" class="text-center">
+  <h2 class="text-center text-lg">TV Watch List</h2>
+  <div v-if="movieWatchList.length === 0" class="text-center">
+    You have nothing in your movie watch list yet...
+  </div>
+  <div v-for="(tv, key) in tvWatchList" v-else :key="key" class="text-center">
     <TVPosterCard :key="key" :index="key" :tv="tv" />
   </div>
 </template>
@@ -14,12 +46,10 @@
 import { defineComponent } from "vue";
 import { movieWatchList, tvWatchList } from "@/functions/useWatchList";
 import TVPosterCard from "@/components/TVPosterCard.vue";
-import MoviePosterCard from "@/components/MoviePosterCard.vue";
 
 export default defineComponent({
   components: {
     TVPosterCard,
-    MoviePosterCard,
   },
   setup() {
     return { movieWatchList, tvWatchList };
