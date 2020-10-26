50184219aefa1045aacc54178af6234df16de9b4
Alex Chiu <alexchiu11@gmail.com>
2020-10-26 16:53:18 +0000

    Setup movie/tv cards for watchlist

--- src/views/WatchList.vue
+++ src/views/WatchList.vue
@@ -1,17 +1,28 @@
 <template>
-  <h1 class="text-center">Your watch list</h1>
-  <div v-for="(movie, index) in movieWatchList" :key="index">
-    {{ movie.title }}
+  <h2 class="text-center">Movies Watch List</h2>
+  <div v-for="(movie, key) in movieWatchList" :key="key" class="text-center">
+    <MoviePosterCard :key="key" :index="key" :movie="movie" />
+  </div>
+
+  <h2 class="text-center">TV Watch List</h2>
+  <div v-for="(tv, key) in tvWatchList" :key="key" class="text-center">
+    <TVPosterCard :key="key" :index="key" :tv="tv" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { movieWatchList } from "@/functions/useWatchList";
+import { movieWatchList, tvWatchList } from "@/functions/useWatchList";
+import TVPosterCard from "@/components/TVPosterCard.vue";
+import MoviePosterCard from "@/components/MoviePosterCard.vue";
 
 export default defineComponent({
+  components: {
+    TVPosterCard,
+    MoviePosterCard,
+  },
   setup() {
-    return { movieWatchList };
+    return { movieWatchList, tvWatchList };
   },
 });
 </script>
